e7dd3bb20daabf6df11a83a973e3d84d09c6a4c1
林桂涛 <852951042@qq.com>
2022-07-11 10:22:16 +0800

    ✨ [box-shadow]利用动画和JS实现简单分页栏

---
+++ 79-利用动画和JS实现简单的分页栏/index.js
@@ -0,0 +1,26 @@
+let prevLink = document.querySelector(".prev");
+let nextLink = document.querySelector(".next");
+let pagination = document.querySelector(".pagination");
+let pageNumberLinks = document.querySelectorAll(".page-number a");
+let maxPageIndex = pageNumberLinks.length - 1;
+pageNumberLinks.forEach((pageNumberLink, activeIndex) => {
+  pageNumberLink.addEventListener("click", () => {
+    pageNumberLinks.forEach((pageNumberLink) => pageNumberLink.parentElement.classList.remove("active"));
+    pageNumberLink.parentElement.classList.add("active");
+    pagination.style.setProperty("--active-index", `${activeIndex}`);
+  });
+});
+prevLink.addEventListener("click", () => {
+  pageNumberLinks.forEach((pageNumberLink) => pageNumberLink.parentElement.classList.remove("active"));
+  let activeIndex = Number(pagination.style.getPropertyValue("--active-index"));
+  activeIndex = activeIndex > 0 ? activeIndex - 1 : 0;
+  pageNumberLinks[activeIndex].parentElement.classList.add("active");
+  pagination.style.setProperty("--active-index", `${activeIndex}`);
+});
+nextLink.addEventListener("click", () => {
+  pageNumberLinks.forEach((pageNumberLink) => pageNumberLink.parentElement.classList.remove("active"));
+  let activeIndex = Number(pagination.style.getPropertyValue("--active-index"));
+  activeIndex = activeIndex < maxPageIndex ? activeIndex + 1 : maxPageIndex;
+  pageNumberLinks[activeIndex].parentElement.classList.add("active");
+  pagination.style.setProperty("--active-index", `${activeIndex}`);
+});
